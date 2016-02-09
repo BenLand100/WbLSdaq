a06d20fbc9e6697d4ddba4efc54d20365bf37bc7
Benjamin Land <benland100@gmail.com>
2016-02-09 15:53:30 -0800

    Add code to handle event skipping in the case of hopelessly mangled LVDS patterns.
    N.B. need to fix/make the trigger counter -> master card cable

--- src/eventmapper.cxx
+++ src/eventmapper.cxx
@@ -50,17 +50,17 @@ int main(int argc, char **argv) {
     const uint16_t test_mask = 0xEF; // bit 4 acts up on master, only 8 bits of counter
     // The bitmask applied before magnitude comparison when patterns don't match up
     const uint16_t comp_mask = 0x0F; // if they don't match... compare the first four
-    // If the magnitude is greater than this assume the count rolled over (sould be less than max_orphans)
+    // If the magnitude is greater than this assume the count rolled over
     const size_t max_offset = 8;  
     // If more than this many orphans are found in a row, assume something went wrong and abort
-    const size_t max_orphans = 4;
+    const size_t max_orphans = 8;
     
     string master_group = "/master/ch0/";
     string fast_group = "/fast/gr0/";
     
     vector<event> events;
     list<locator> master_overflow, fast_overflow;
-    size_t orphans = 0, master_orphans = 0, fast_orphans = 0, total_events = 0;
+    size_t skipped = 0, total_skipped = 0, orphans = 0, master_orphans = 0, fast_orphans = 0, total_events = 0;
     
     //loop over files
     for (size_t fidx = startidx; fidx <= endidx; fidx++) {
@@ -98,31 +98,56 @@ int main(int argc, char **argv) {
             
             // One of the two triggers might be an orphan
             if ((ev.master.pattern & test_mask) != (ev.fast.pattern & test_mask)) {
-                cout << "Orphan found - master_file: " << ev.master.file << " master_index:" << ev.master.index;
+                
+                cout << "Discontinuity found - master_file: " << ev.master.file << " master_index:" << ev.master.index;
                 cout << " fast_file: " << ev.fast.file << " fast_index:" << ev.fast.index << endl;
-                cout << "\tmaster_pattern: " << (ev.master.pattern & comp_mask) << " fast_pattern: " << (ev.fast.pattern & comp_mask) << endl;
+                cout << "\tmaster_pattern: " << (ev.master.pattern & 0xFF) << " / " << (ev.master.pattern & test_mask) << " / " << (ev.master.pattern & comp_mask); 
+                cout << " fast_pattern: " << (ev.fast.pattern & 0xFF) << " / " << (ev.fast.pattern & test_mask) << " / " << (ev.fast.pattern & comp_mask) << endl;
+               
+                int mdelta = (ev.master.pattern) - (events.back().master.pattern);
+                int fdelta = (ev.fast.pattern) - (events.back().fast.pattern);
+                int delta = (ev.fast.pattern & comp_mask) - (ev.master.pattern & comp_mask);
+                
+                if (skipped && abs(ev.fast.pattern-ev.master.pattern) < max_offset) {
+                    cout << "\tPossibly back on track..." << endl;
+                } else if ((abs(delta) != 1) && (abs(mdelta) > max_offset+skipped || abs(fdelta) > max_offset+skipped)) {
+                    cout << "\tWay off track - skipping (" << skipped << ") events as last ditch effort" << endl;
+                    skipped++;
+                    total_skipped++;
+                    if (skipped > 255) {
+                        cout << "\tWe skipped more events than we can count, bailing out." << endl;
+                        return -1;
+                    }
+                    continue;
+                }    
+                skipped = 0;
+            
                 bool invert = abs((ev.fast.pattern & comp_mask) - (ev.master.pattern & comp_mask)) > max_offset;
-                if (((ev.fast.pattern & comp_mask) > (ev.master.pattern & comp_mask)) != invert) {
-                    cout << "\tmaster was orphaned" << endl;
-                    fast_overflow.push_front(ev.fast);
-                    ev.fast.file = -1;
-                    ev.fast.index = -1;
-                    ev.fast.pattern = -1;
-                    master_orphans++;
-                } else {
-                    master_overflow.push_front(ev.master);
-                    cout << "\tfast was orphand" << endl;
-                    ev.master.file = -1;
-                    ev.master.index = -1;
-                    ev.master.pattern = -1;
-                    fast_orphans++;
-                }
+                    if (((ev.fast.pattern & comp_mask) > (ev.master.pattern & comp_mask)) != invert) {
+                        cout << "\tmaster was orphaned" << endl;
+                        fast_overflow.push_front(ev.fast);
+                        ev.fast.file = -1;
+                        ev.fast.index = -1;
+                        ev.fast.pattern = -1;
+                        master_orphans++;
+                    } else {
+                        master_overflow.push_front(ev.master);
+                        cout << "\tfast was orphaned" << endl;
+                        ev.master.file = -1;
+                        ev.master.index = -1;
+                        ev.master.pattern = -1;
+                        fast_orphans++;
+                    }
                 orphans++;
                 if (orphans > max_orphans) {
                     cout << "Too many orphans in a row - bailing out." << endl;
                     return 1;
                 }
             } else {
+                //cout << "Good event - master_file: " << ev.master.file << " master_index:" << ev.master.index;
+                //cout << " fast_file: " << ev.fast.file << " fast_index:" << ev.fast.index << endl;
+                //cout << "\tmaster_pattern: " << (ev.master.pattern & 0xFF) << " / " << (ev.master.pattern & test_mask) << " / " << (ev.master.pattern & comp_mask);
+                //cout << " fast_pattern: " << (ev.fast.pattern & 0xFF) << " / " << (ev.fast.pattern & test_mask) << " / " << (ev.fast.pattern & comp_mask) << endl;
                 orphans = 0;
             }
              
@@ -149,7 +174,7 @@ int main(int argc, char **argv) {
         
     }
     
-    cout << "Events: " << total_events << ", Master Orphans: " << master_orphans << ", Fast Orphans: " << fast_orphans << endl;
+    cout << "Events: " << total_events << ", Master Orphans: " << master_orphans << ", Fast Orphans: " << fast_orphans << ", Skipped: " << total_skipped << endl;
     
     //May have some orphans in one or the other overflow (but not both so no checks for correlations)
     while (master_overflow.size()) {
