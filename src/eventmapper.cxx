bd28e3d06745d0ba065fe96f2eec32d7a6fac979
Benjamin Land <benland100@gmail.com>
2016-02-10 17:40:02 -0800

    Make eventmapper more useful.

--- src/eventmapper.cxx
+++ src/eventmapper.cxx
@@ -3,8 +3,11 @@
 #include <fstream>
 #include <vector>
 #include <list>
+#include <cstring>
 #include <string>
 #include <math.h>
+#include <unistd.h>
+#include <glob.h>
 
 using namespace std;
 using namespace H5;
@@ -32,40 +35,131 @@ typedef struct {
 } event;
 
 
+[[noreturn]] void help() {
+    cout << "eventmapper reads all files that match `${prefix}.${index}.h5` ";
+    cout << "respecting the optional bounds on the index and generates ";
+    cout << "an event map as `${prefix}.map.csv` containg correlated events ";
+    cout << "determined by scanning the files and matching sequential events ";
+    cout << "that are equal according to the test mask or ophans which are ";
+    cout << "the lesser of two events according to the comparison mask excepting ";
+    cout << "cases where the offset according to the comparion mask is greater ";
+    cout << "than the specified max offset and the larger is orphaned. In ";
+    cout << "exceptional cases (with -r) when the raw offset is more than the max offset ";
+    cout << "and the patterns do not pass equality events from both cards will ";
+    cout << "be skipped to try to get back on track. This only happens when ";
+    cout << "there are faulty connections on the LVDS inputs." << endl; 
+    cout << "./eventmapper [options] prefix" << endl;
+    cout << "\t-v            enable verbose mode" << endl;
+    cout << "\t-r            resync by skipping events until things look right" << endl;
+    cout << "\t-s index      force start file index [least]" << endl;
+    cout << "\t-e index      force end file index [greatest]" << endl;
+    cout << "\t-t mask       set equality test mask [0xEF]" << endl;
+    cout << "\t-c mask       set comparison mask [0x0F]" << endl;
+    cout << "\t-o offset     set max offset used for comparison [8]" << endl;
+    cout << "\t-m group      set master pattern group [/fast/gr0/]" << endl;
+    cout << "\t-f group      set fast pattern group [/master/ch0/]" << endl;
+    exit(1);
+}
+
 // Assumes two cards (fast w/ g0 patterns, master w/ c0 petterns) with card-wide 
 // triggers that have stored LVDS patterns of which some bits are an external
 // trigger count. Missed triggers will have -1 in the locator fields.
 int main(int argc, char **argv) {
+    
+    // The bitmask used to compare two patterns for equality
+    uint16_t test_mask = 0xEF; // bit 4 acts up on master, only 8 bits of counter
+    // The bitmask applied before magnitude comparison when patterns don't match up
+    uint16_t comp_mask = 0x0F; // if they don't match... compare the first four
+    // If the magnitude is greater than this assume the count rolled over
+    size_t max_offset = 8;
+    // Group to read master patterns from
+    string master_group = "/master/ch0/";
+    // Group to read fast patterns from
+    string fast_group = "/fast/gr0/";
+    // Start index
+    int startidx = -1;
+    // End index
+    int endidx = -1;
+    // File prefix to read from
+    string fprefix;
+    // enable skipping to resync
+    bool resync = false;
+    // Extra debug flag
+    bool verbose = false;
 
-    if (argc != 4) {
-        cout << "./eventmapper fileprefix startidx endidx" << endl;
-        return 1;
+    opterr = 0;
+    int c;
+    while ((c = getopt(argc, argv, ":s:e:vrt:c:o:m:f:")) != -1) {
+        switch (c) {
+            case 's':
+                startidx = stoull(optarg,NULL,0);
+                break;
+            case 'e':
+                endidx = stoull(optarg,NULL,0);
+                break;
+            case 'v':
+                verbose = true;
+                break;
+            case 'r':
+                resync = true;
+                break;
+            case 't':
+                test_mask = stoul(optarg,NULL,0) & 0xFFFF;
+                break;
+            case 'c':
+                comp_mask = stoul(optarg,NULL,0) & 0xFFFF;
+                break;
+            case 'o':
+                max_offset = stoull(optarg,NULL,9);
+                break;
+            case 'm':
+                master_group = string(optarg);
+                break;
+            case 'f':
+                fast_group = string(optarg);
+                break;
+            case ':':
+                cout << "-" << optopt << " requires an argument" << endl;
+                help();
+            case '?':
+                cout << "-" << optopt << " is an unknown option" << endl;
+                help();
+            default:
+                cout << "Unexpected result from getopt" << endl;
+                help();
+        }
     }
 
-    string fprefix(argv[1]);
-    const size_t startidx = atoi(argv[2]);
-    const size_t endidx = atoi(argv[3]);
+    if (argc - optind != 1) help();
+    fprefix = argv[optind];
 
-    // The bitmask used to compare two patterns for equality
-    const uint16_t test_mask = 0xEF; // bit 4 acts up on master, only 8 bits of counter
-    // The bitmask applied before magnitude comparison when patterns don't match up
-    const uint16_t comp_mask = 0x0F; // if they don't match... compare the first four
-    // If the magnitude is greater than this assume the count rolled over
-    const size_t max_offset = 8;  
-    // If more than this many orphans are found in a row, assume something went wrong and abort
-    const size_t max_orphans = 8;
-    
-    string master_group = "/master/ch0/";
-    string fast_group = "/fast/gr0/";
-    
+    if (startidx < 0 || endidx < 0) {
+        glob_t files;
+        string pattern = fprefix+".*.h5";
+        glob(pattern.c_str(),0,NULL,&files);
+        size_t prelen = fprefix.length();
+        size_t min = INT_MAX, max = 0;
+        for (size_t i = 0; i < files.gl_pathc; i++) {
+            int len = strlen(files.gl_pathv[i]);
+            string fname(&(files.gl_pathv[i])[prelen+1],len-prelen-4);
+            size_t j = stoull(fname);
+            if (j < min) min = j;
+            if (j > max) max = j;
+        }
+        if (startidx < 0) startidx = min;
+        if (endidx < 0) endidx = max;
+    }
+
     vector<event> events;
     list<locator> master_overflow, fast_overflow;
     size_t skipped = 0, total_skipped = 0, orphans = 0, master_orphans = 0, fast_orphans = 0, total_events = 0;
     
     //loop over files
-    for (size_t fidx = startidx; fidx <= endidx; fidx++) {
+    for (int fidx = startidx; fidx <= endidx; fidx++) {
     
-        H5File file(fprefix+"."+to_string(fidx)+".h5", H5F_ACC_RDONLY);
+        string fname = fprefix+"."+to_string(fidx)+".h5";
+        if (verbose) cout << "Opening file " << fname << endl;
+        H5File file(fname, H5F_ACC_RDONLY);
         
         vector<uint16_t> master_patterns, fast_patterns;
         getPatterns(file, master_group, master_patterns);
@@ -103,51 +197,55 @@ int main(int argc, char **argv) {
                 cout << " fast_file: " << ev.fast.file << " fast_index:" << ev.fast.index << endl;
                 cout << "\tmaster_pattern: " << (ev.master.pattern & 0xFF) << " / " << (ev.master.pattern & test_mask) << " / " << (ev.master.pattern & comp_mask); 
                 cout << " fast_pattern: " << (ev.fast.pattern & 0xFF) << " / " << (ev.fast.pattern & test_mask) << " / " << (ev.fast.pattern & comp_mask) << endl;
-               
-                int mdelta = (ev.master.pattern) - (events.back().master.pattern);
-                int fdelta = (ev.fast.pattern) - (events.back().fast.pattern);
-                int delta = (ev.fast.pattern & comp_mask) - (ev.master.pattern & comp_mask);
                 
-                if (skipped && abs(ev.fast.pattern-ev.master.pattern) < max_offset) {
-                    cout << "\tPossibly back on track..." << endl;
-                } else if ((abs(delta) != 1) && (abs(mdelta) > max_offset+skipped || abs(fdelta) > max_offset+skipped)) {
-                    cout << "\tWay off track - skipping (" << skipped << ") events as last ditch effort" << endl;
-                    skipped++;
-                    total_skipped++;
-                    if (skipped > 255) {
-                        cout << "\tWe skipped more events than we can count, bailing out." << endl;
-                        return -1;
-                    }
-                    continue;
-                }    
-                skipped = 0;
+                if (resync) {
+                    int mdelta = (ev.master.pattern) - (events.back().master.pattern);
+                    int fdelta = (ev.fast.pattern) - (events.back().fast.pattern);
+                    int delta = (ev.fast.pattern & comp_mask) - (ev.master.pattern & comp_mask);
+                
+                    if (skipped && abs(ev.fast.pattern-ev.master.pattern) < max_offset) {
+                        cout << "\tPossibly back on track..." << endl;
+                    } else if ((abs(delta) != 1) && (abs(mdelta) > max_offset+skipped || abs(fdelta) > max_offset+skipped)) {
+                        cout << "\tWay off track - skipping (" << skipped << ") events as last ditch effort" << endl;
+                        skipped++;
+                        total_skipped++;
+                        if (skipped > 255) {
+                            cout << "\tWe skipped more events than we can count, bailing out." << endl;
+                            return -1;
+                        }
+                        continue;
+                    }    
+                    skipped = 0;
+                }
             
                 bool invert = abs((ev.fast.pattern & comp_mask) - (ev.master.pattern & comp_mask)) > max_offset;
-                    if (((ev.fast.pattern & comp_mask) > (ev.master.pattern & comp_mask)) != invert) {
-                        cout << "\tmaster was orphaned" << endl;
-                        fast_overflow.push_front(ev.fast);
-                        ev.fast.file = -1;
-                        ev.fast.index = -1;
-                        ev.fast.pattern = -1;
-                        master_orphans++;
-                    } else {
-                        master_overflow.push_front(ev.master);
-                        cout << "\tfast was orphaned" << endl;
-                        ev.master.file = -1;
-                        ev.master.index = -1;
-                        ev.master.pattern = -1;
-                        fast_orphans++;
-                    }
+                if (((ev.fast.pattern & comp_mask) > (ev.master.pattern & comp_mask)) != invert) {
+                    cout << "\tmaster was orphaned" << endl;
+                    fast_overflow.push_front(ev.fast);
+                    ev.fast.file = -1;
+                    ev.fast.index = -1;
+                    ev.fast.pattern = -1;
+                    master_orphans++;
+                } else {
+                    master_overflow.push_front(ev.master);
+                    cout << "\tfast was orphaned" << endl;
+                    ev.master.file = -1;
+                    ev.master.index = -1;
+                    ev.master.pattern = -1;
+                    fast_orphans++;
+                }
                 orphans++;
-                if (orphans > max_orphans) {
+                if (orphans > max_offset) {
                     cout << "Too many orphans in a row - bailing out." << endl;
                     return 1;
                 }
             } else {
-                //cout << "Good event - master_file: " << ev.master.file << " master_index:" << ev.master.index;
-                //cout << " fast_file: " << ev.fast.file << " fast_index:" << ev.fast.index << endl;
-                //cout << "\tmaster_pattern: " << (ev.master.pattern & 0xFF) << " / " << (ev.master.pattern & test_mask) << " / " << (ev.master.pattern & comp_mask);
-                //cout << " fast_pattern: " << (ev.fast.pattern & 0xFF) << " / " << (ev.fast.pattern & test_mask) << " / " << (ev.fast.pattern & comp_mask) << endl;
+                if (verbose) {
+                    cout << "Good event - master_file: " << ev.master.file << " master_index:" << ev.master.index;
+                    cout << " fast_file: " << ev.fast.file << " fast_index:" << ev.fast.index << endl;
+                    cout << "\tmaster_pattern: " << (ev.master.pattern & 0xFF) << " / " << (ev.master.pattern & test_mask) << " / " << (ev.master.pattern & comp_mask);
+                    cout << " fast_pattern: " << (ev.fast.pattern & 0xFF) << " / " << (ev.fast.pattern & test_mask) << " / " << (ev.fast.pattern & comp_mask) << endl;
+                }
                 orphans = 0;
             }
              
